432068991f827295c41b81bc71d182e391aedf66
vrtex <kuba.jakub17@gmail.com>
2020-02-12 18:53:01 +0100

    Nah

--- src/main/java/bongo/controller/QueryController.java
+++ src/main/java/bongo/controller/QueryController.java
@@ -61,7 +61,7 @@ public class QueryController
     public String postCustomQuery(@Validated @ModelAttribute CustomQueryModel customQueryModel, BindingResult result, Model model, RedirectAttributes redirectAttributes) throws SQLException
     {
         if(isQueryForbidden(customQueryModel.getCustomQuery()))
-            return handleErrors(model, result, new Exception("that's an illegal query. Not nice"));
+            return "redirect:/query";
         if (customQueryModel.getCustomQuery().toLowerCase().contains("insert") || customQueryModel.getCustomQuery().toLowerCase().contains("update") || customQueryModel.getCustomQuery().toLowerCase().contains("drop")) {
             try {
                 dataBaseService.insertUpdateQuery(customQueryModel.getCustomQuery());
